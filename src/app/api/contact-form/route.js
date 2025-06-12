7bd6db42954221f62fa220f6ed17b29d6a8ef242
Muhdsidin <officialzedro@gmail.com>
2025-06-12 18:42:28 +0530

    update : create contact form api

---
+++ src/app/api/contact-form/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import Form from "../../../model/Form-model"; // Adjust if path is wrong
+import { connectDB } from '../../../config/database';
+
+export async function POST(req) {
+  try {
+    const data = await req.json();
+    const { name, message, email, phone , activity } = data;
+
+    if (!name || !message || !email || !phone) {
+      return NextResponse.json({
+        message: "All fields are required",
+        success: false
+      }, { status: 400 });
+      
+    }
+
+    await connectDB();
+
+    const formUpload = await Form.create({ name, message, email, phone  , activity });
+    console.log(formUpload);
+
+    return NextResponse.json({
+      message: "Upload successfully",
+      success: true
+    }, { status: 201 });
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      message: "Internal server error",
+      error: error.message,
+      success: false
+    }, { status: 500 });
+  }
+}
