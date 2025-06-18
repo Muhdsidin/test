7b2a79cba937235ed477966e1e5e1d47c4444487
Muhdsidin <officialzedro@gmail.com>
2025-06-18 23:44:36 +0530

    added : activity Feature

---
+++ src/app/api/business-activity/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import { connectDB } from '../../../config/database';
+import ActivityModel from '../../../model/Activity-model';
+
+export async function POST (req){
+    try {
+        const data = await req.json();
+        const {Name , Email , Message , Phone , Activity , Details} = data;
+
+        if(!Name || !Email || !Message || !Phone || !Activity || !Details){
+            return NextResponse.json({message:"All fields are required"},{status:400});
+        }
+        await connectDB();
+        const activity = await ActivityModel.create({name : Name , email : Email , message : Message , phone : Phone , activity : Activity , details : Details});
+        return NextResponse.json({
+            success : true,
+            message : "succesFully uploaded "
+        });
+        
+    } catch (error) {
+         console.error(error);
+            return NextResponse.json({
+              message: "Internal server error",
+              error: error.message,
+              success: false
+            }, { status: 500 });
+    }
+}
